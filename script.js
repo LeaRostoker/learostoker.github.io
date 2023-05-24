17151d3e66f4e26d745574b7b2bda84711912c5a
LeaRostoker <lea.rostoker.12@gmail.com>
2023-05-24 12:20:36 +0200

    Add bubble game

--- script.js
+++ script.js
@@ -35,6 +35,59 @@ window.addEventListener("DOMContentLoaded", () => {
       });
     });
   });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    // Define the number of bubbles to create
+    const NUM_BUBBLES = 10;
+  
+    let bubbles = [];
+  
+    // Create the bubbles
+    for (let i = 0; i < NUM_BUBBLES; i++) {
+      let bubble = document.createElement("div");
+      bubble.classList.add("bubble");
+      bubble.style.top = Math.floor(Math.random() * 80) + "%";
+      bubble.style.left = Math.floor(Math.random() * 80) + "%";
+      document.getElementById("bubble-container").appendChild(bubble);
+      bubbles.push(bubble);
+    }
+  
+    // Add a click event listener to each bubble
+    for (let i = 0; i < bubbles.length; i++) {
+      bubbles[i].addEventListener("click", function() {
+        this.style.animation = "none";
+        this.style.backgroundColor = "red";
+        this.classList.remove("moving");
+      });
+    }
+  
+    // Move the bubbles around the screen
+    setInterval(function() {
+      for (let i = 0; i < bubbles.length; i++) {
+        let top = parseInt(bubbles[i].style.top);
+        let left = parseInt(bubbles[i].style.left);
+        let newTop = top + Math.floor(Math.random() * 10) - 5;
+        let newLeft = left + Math.floor(Math.random() * 10) - 5;
+  
+        if (newTop < 0 || newTop > 80) {
+          newTop = top - Math.floor(Math.random() * 10);
+        }
+        if (newLeft < 0 || newLeft > 80) {
+          newLeft = left - Math.floor(Math.random() * 10);
+        }
+  
+        bubbles[i].style.top = newTop + "%";
+        bubbles[i].style.left = newLeft + "%";
+  
+        if (bubbles[i].classList.contains("moving")) {
+          bubbles[i].classList.remove("moving");
+        } else {
+          bubbles[i].classList.add("moving");
+        }
+      }
+    }, 1000);
+  });
+  
   
 
   
